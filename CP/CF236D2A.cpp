4e2f1f7d47579691ab34dec5981515dd7f6efe7b
miguelsndc <mglsndc@gmail.com>
2024-04-21 21:11:54 -0300

    Boy or Girl

--- CP/CF236D2A.cpp
+++ CP/CF236D2A.cpp
@@ -1,10 +1,32 @@
- <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    string s;
+    getline(cin, s);
+
+    vector<char> ch_list;
+    for (char &i: s) {
+        bool found = false;
+        for (char &j: ch_list) {
+            if (i == j) {
+                found = true;
+            }
+        }
+        if(!found) {
+            ch_list.push_back(i);
+        }
+    }
+
+    if (ch_list.size() % 2 != 0) {
+        cout << "IGNORE HIM!";
+    } else {
+        cout << "CHAT WITH HER!";
+    }
+
     return 0;
 }
