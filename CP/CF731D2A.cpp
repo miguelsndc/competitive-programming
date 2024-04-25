cec0b1a3c359d3fe97311696038a95b68c7e2d44
miguelsndc <mglsndc@gmail.com>
2024-04-24 23:04:37 -0300

    Night at the Museum

--- CP/CF731D2A.cpp
+++ CP/CF731D2A.cpp
@@ -1,10 +1,23 @@
- <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    string s;
+    getline(cin, s);
+
+    int p = 0;
+    int moves = 0;
+    for (char &i: s) {
+        int m = i - 97;
+        int walk = abs(p - m);
+        moves += walk <= (26 - walk) ? walk : (26 - walk);
+        p = m;
+    }
+    cout << moves;
+
     return 0;
 }
