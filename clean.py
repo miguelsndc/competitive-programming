1e5ff0c869e39522c8e7561dda7749b8d14db095
miguelsndc <mglsndc@gmail.com>
2024-04-28 20:59:24 -0300

    python script to clean vscode junk

---
+++ clean.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+bad_extensions = ['.prob', '.bin', '.exe', '.c.swp']
+bad_dirs = ['.cph', 'build', 'Debug']
+exclude = ['.vscode', '.git']
+
+for root, dirs, files in os.walk('.'):
+    for f in exclude:
+        if f in dirs:
+            dirs.remove(f)
+
+    for file in files:
+        if file.endswith(tuple(bad_extensions)):
+            path = os.path.join(root, file)
+            if os.path.exists(path):
+                os.remove(path)
+    
+    for dir in dirs:
+        if dir.endswith(tuple(bad_dirs)):
+            dir_path = os.path.join(root, dir)
+            if os.path.exists(dir_path):
+                shutil.rmtree(dir_path)
+
+
