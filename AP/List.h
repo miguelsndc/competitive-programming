3f69c4ef7e324b3a37f8159d225dd9116f93bb76
miguelsndc <mglsndc@gmail.com>
2024-04-28 19:18:33 -0300

    AP3 done - array based list

---
+++ AP/List.h
@@ -0,0 +1,54 @@
+#ifndef LIST_BASE_CLASS
+#define LIST_BASE_CLASS
+#endif
+
+template <typename E>
+class List
+{
+private:
+    void operator=(const List &) {}
+    List(const List &);
+
+public:
+    List() {}
+    virtual ~List() {}
+
+    // Clear contents from the list, to make it empty
+    virtual void clear() = 0;
+
+    // Insert an element at the current position
+    // item: The element to be inserted;
+    virtual void insert(const E &item) = 0;
+
+    // Append an element to the end of the lsit
+    // item: the element to me appended
+    virtual void append(const E &item) = 0;
+
+    // Remove and return the current element
+    // Return: the element that was removed
+    virtual E remove() = 0;
+
+    // Set the current position to the start of the list
+    virtual void moveToStart() = 0;
+
+    // Set the current position to the end of the list
+    virtual void moveToEnd() = 0;
+
+    // Move the current position one step left. No change if already at beginning.
+    virtual void prev() = 0;
+
+    // Move the current position one step right. No change if already at end.
+    virtual void next() = 0;
+
+    // Return: the number of elements in the list;
+    virtual int length() const = 0;
+
+    // Return: the position of the current element in the list;
+    virtual int currPos() const = 0;
+
+    // Set the current position.
+    // pos: The position to make current.
+    virtual void moveToPos(int pos) = 0;
+
+    virtual const E &getValue() const = 0;
+};
--- implementations/data-structures/ArrayList.cpp
+++ implementations/data-structures/ArrayList.cpp
@@ -4,26 +4,31 @@
 
 using namespace std;
 
-void Assert(bool expr, const string& msg) {
-    if (!expr) {
+void Assert(bool expr, const string &msg)
+{
+    if (!expr)
+    {
         throw runtime_error(msg);
     }
 }
 
 template <typename E>
-class ArrayList: public List<E> {
+class ArrayList : public List<E>
+{
 private:
     int maxSize;
     int listSize;
     int curr;
-    E* listArray;
+    E *listArray;
 
-    void increaseCapacity() {
+    void increaseCapacity()
+    {
         int newCapacity = ceil(maxSize * 1.7);
         int oldCurr = curr;
         E *newListArray = new E[newCapacity];
 
-        for (this->moveToStart(); this->currPos() < this->length(); this->next()) {
+        for (this->moveToStart(); this->currPos() < this->length(); this->next())
+        {
             newListArray[curr] = getValue();
         }
 
@@ -33,8 +38,10 @@ private:
         curr = oldCurr;
         listArray = newListArray;
     }
+
 public:
-    explicit ArrayList(int size=1) {
+    explicit ArrayList(int size = 1)
+    {
         maxSize = size;
         listSize = curr = 0;
         listArray = new E[maxSize];
@@ -45,83 +52,97 @@ public:
 
     ~ArrayList() override { delete[] listArray; };
 
-
-    void clear() override {
+    void clear() override
+    {
         delete[] listArray;
         listSize = curr = 0;
         listArray = new E[maxSize];
     }
 
-    void insert(const E& it) override {
-        if (listSize >= maxSize) {
+    void insert(const E &it) override
+    {
+        if (listSize >= maxSize)
+        {
             increaseCapacity();
         }
-        for (int i = listSize; i > curr; i--) {
+        for (int i = listSize; i > curr; i--)
+        {
             listArray[i] = listArray[i - 1]; // shifting elements up
         }
         listArray[curr] = it;
         listSize++;
     }
 
-    void append(const E& it) override{
-        if (listSize >= maxSize) {
+    void append(const E &it) override
+    {
+        if (listSize >= maxSize)
+        {
             increaseCapacity();
         }
         listArray[listSize++] = it;
     }
 
     // Remove and return the current element
-    E remove() override{
+    E remove() override
+    {
         Assert((curr >= 0) && (curr < listSize), "NO element");
         E it = listArray[curr];
-        for (int i = curr; i < listSize - 1; i++) {
+        for (int i = curr; i < listSize - 1; i++)
+        {
             listArray[i] = listArray[i + 1];
         }
         listSize--;
         return it;
     }
 
-    void moveToStart() override{
+    void moveToStart() override
+    {
         curr = 0;
     }
 
-    void moveToEnd() override{
+    void moveToEnd() override
+    {
         curr = listSize;
     }
 
-    void prev() override{
-        if (curr != 0) {
+    void prev() override
+    {
+        if (curr != 0)
+        {
             curr--;
         }
     }
 
-    void next() override{
-        if(curr < listSize) {
+    void next() override
+    {
+        if (curr < listSize)
+        {
             curr++;
         }
     }
 
-    int length() const override{ return listSize; }
-    int currPos() const override{ return curr; }
+    int length() const override { return listSize; }
+    int currPos() const override { return curr; }
 
-    void moveToPos(int pos) override{
+    void moveToPos(int pos) override
+    {
         Assert((pos >= 0) && (pos <= listSize), "Position out of range");
         curr = pos;
     }
 
-    const E& getValue() const override{
-        Assert(( curr >= 0 ) && (curr <= listSize), "No current element");
+    const E &getValue() const override
+    {
+        Assert((curr >= 0) && (curr <= listSize), "No current element");
         return listArray[curr];
     }
-
-
 };
 
-
-int main() {
+int main()
+{
     auto *list = new ArrayList<int>(1);
 
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < 25; i++)
+    {
         list->insert(i * i);
         cout << list->getMaxSize() << '\n';
     }
