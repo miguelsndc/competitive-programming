7a4d7fc507546b4214b07fb018e779d0fe14c0e0
miguelsndc <mglsndc@gmail.com>
2024-05-09 16:40:28 -0300

    Trying to automatically fix it (ta esquisito)

--- Special/matching_brackets.cpp
+++ Special/matching_brackets.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 // the idea here is to decide whether the brackets are correctly matched
-// for example, ({[]}) is correct matching, but (({[]}) isn't, here we use parentheses,
-// curly and square brackets.
+// for example, ({[]}) is correctly matched, but (({[]}) isn't,
+// here we use parentheses, curly and square brackets.
 
 // input: a string of length `n` containing the brackets. 1 <= n <= 100
 // user should output "1" if it's correctly matching or "0" otherwise.
 
 // { [ ( ( ) ) ] }
+
+// can i output a corrected string ?
 
 int main()
 {
@@ -22,11 +24,14 @@ int main()
         {'[', ']'},
         {'(', ')'},
     };
-    stack<char> pile;
 
-    int matches = 1;
-    for (auto ch : example)
+    stack<char> pile;
+    queue<pair<int, int>> positions;
+
+    int index = -1;
+    for (int i = 0; i < example.size(); i++)
     {
+        char ch = example[i];
         if (bracket_map.count(ch) > 0) // opening
             pile.push(ch);
         else if (bracket_map[pile.top()] == ch) // closing and matches
@@ -35,12 +40,53 @@ int main()
         }
         else // closing and doesn't match
         {
-            matches = 0;
-            break;
+            index = i;
+            positions.push(make_pair(ch, i)); // save character and position for later correction
         }
     }
 
-    cout << matches;
+    if (positions.size() == 0 && pile.size() == 0) // string is ok, output -1 and quit.
+    {
+        cout << -1;
+        return 0;
+    }
+
+    // characters left on stack: opens but never closes ->
+    // two options:
+    // - append immediately after
+    // - append at the end
+    // characters on position[]: closes without opening
+    // let's just prepend the opening bracket.
+
+    int new_size = example.size() + positions.size() + pile.size();
+    // input: {{)]
+    // output: {{()[]}}
+    string corrected(new_size, ' ');
+
+    int popped = 0;
+    for (int i = 0; i < (example.size() + positions.size()); i++)
+    {
+        if ((positions.front().second) == i) // we're in the oposition immediately before the closing char
+        {
+            char closing = positions.front().first;
+            char open;
+            for (auto const &[opening, val] : bracket_map) // find corresponding key (can i use two-way mapping?)
+            {
+                if (val == closing)
+                {
+                    open = opening;
+                    break;
+                }
+            }
+            corrected[i] = open;
+            popped++;
+            positions.pop();
+        }
+        else
+        {
+            corrected[i] = example[i - popped];
+        }
+    }
 
     return 0;
 }
