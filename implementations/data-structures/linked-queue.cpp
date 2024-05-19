fe9442215d705dff1ba8c37a80b4c3684e0f8b8d
miguelsndc <mglsndc@gmail.com>
2024-05-19 10:55:55 -0300

    add frontvalue method

--- implementations/data-structures/linked-queue.cpp
+++ implementations/data-structures/linked-queue.cpp
@@ -1,4 +1,20 @@
 template <typename E>
+class Link {
+
+public:
+    E element;
+    Link *next;
+
+    Link(const E &elemval, Link *nextval = NULL) {
+        element = elemval;
+        next = nextval;
+    }
+    Link(Link *nextval = NULL) {
+        next = nextval;
+    }
+};
+
+template <typename E>
 class Queue {
 private:
     Link<E> *front;
@@ -33,6 +49,12 @@ public:
         delete temp;
         size--;
         return item;
+    }
+
+    E frontValue() {
+        if (size > 0) {
+            return front->next->element;
+        }
     }
     int length() const { return size; }
 };
