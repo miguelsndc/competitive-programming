304a7e09d264b2b02c60b55f4e8953f515e7ac07
miguelsndc <mglsndc@gmail.com>
2024-05-21 23:03:18 -0300

    Lovely Palindromes

---
+++ CP/CF688B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string base;
+    cin >> base;
+    cout << base;
+    reverse(base.begin(), base.end());
+    cout << base;
+    return 0;
+}
