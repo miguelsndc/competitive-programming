cdf951c0652f30abcc92d5917aab05acd11c6efc
= <=>
2024-05-27 14:58:56 -0300

    .

--- implementations/data-structures/hashtable--open-hashing.cpp
+++ implementations/data-structures/hashtable--open-hashing.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <bits/stdc++.h>
-
+#include <vector>
 using namespace std;
 
 template <typename K, typename E>
@@ -15,121 +14,50 @@ class Dictionary
     virtual int hash(K key) = 0;
 };
 
-// OPEN HASHING
-template <typename K, typename E>
-class Entry
-{
-public:
-    K key;
-    E value;
-    Entry(K k, E v = NULL)
-    {
-        key = k;
-        value = v;
-    }
-};
-
 template <typename K, typename E>
 class HashTable : public Dictionary<K, E>
 {
+
 private:
-    int size = 16;
+    int size = 10;
     int cnt = 0;
-    list<Entry<K, E>> table[];
 
-    void create_dict()
-    {
-        table = new list<Entry<K, E>>[size];
-        for (int i = 0; i < size - 1; i++)
-            table[i] = new list<Entry<K, E>>();
-    }
+    typedef vector<pair<K, E>> list;
+    typedef list *list_ptr;
+    list_ptr table[10];
 
-public:
-    HashTable(int p_size)
-    {
-        size = p_size;
-        create_dict();
-    }
-    HashTable()
-    {
-        create_dict();
-    }
-
-    int hash(int key)
+    int hash(K key)
     {
         return key % size;
     }
 
-    int hash(string key)
+public:
+    HashTable(int p_size = 10)
     {
-        int s = key.length();
-        int sum = 0;
-        for (char ch : key)
-            sum += (int)ch;
-        return abs(sum) % size;
+        size = p_size;
     }
 
     void insert(K key, E value)
     {
-        if (find(key) == NULL)
-        {
-            int pos = hash(key);
-            list<Entry<K, E>> chain = table[pos];
-            Entry<K, E> entry = new Entry<K, E>(key, value);
-            chain.push_back(entry);
-            cnt++;
-        }
+        int pos = hash(key);
+        list *l = table->at(pos);
+        l->push_back(make_pair(key, value));
     }
     E find(K key)
     {
         int pos = hash(key);
-        list<Entry<K, E>> chain = table[pos];
-        for (Entry<K, E> item : chain)
+        list *l = table[pos];
+        for (int i = 0; i < l->size(); i++)
         {
-            if (key == item.key)
-                return item.value;
-        }
-        return NULL;
-    }
-
-    E remove(K key)
-    {
-        int pos = hash(key);
-        list<Entry<K, E>> chain = table[pos];
-        Entry<K, E> val_to_be_removed = NULL;
-        for (Entry<K, E> item : chain)
-        {
-            if (key == item.key)
-                val_to_be_removed = item;
-        }
-        if (val_to_be_removed != NULL)
-        {
-            chain.remove(val_to_be_removed);
-            cnt--;
-        }
-
-        return val_to_be_removed;
-    }
-
-    int length() { return cnt; }
-
-    void clear()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (Entry<K, E> item : table[i])
+            if (l->at(i).first == key)
             {
-                delete item;
+                return l->at(i).second;
             }
-            delete table[i];
         }
-        create_dict();
+    }
+
+    int main()
+    {
+        return 0;
     }
 };
-
-int main()
-{
-    HashTable<string, int> mp;
-
-    return 0;
-}
