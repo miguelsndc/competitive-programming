9c364e21744e4d0c3da7543e89611ed0682f5bd5
miguelsndc <mglsndc@gmail.com>
2024-06-01 20:20:08 -0300

    trabalho honesto

--- CP/CSES1161 Stick Divisions.cpp
+++ CP/CSES1161 Stick Divisions.cpp
@@ -6,8 +6,29 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int x, n;
+    cin >> x >> n;
+    // keep sorted
+    // keep taking the smallest out
+    // priority queue!
+    priority_queue<int, vector<int>, greater<int>> pqueue;
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        pqueue.push(t);
+    }
 
-    // 1 2 3 4 5 6 7 n8
+    ll ans = 0;
+    for (int i = 1; i < n; i++) {
+        int a = pqueue.top();
+        pqueue.pop();
+        int b = pqueue.top();
+        pqueue.pop();
+        pqueue.push(a + b);
+        ans += a + b;
+    }
+
+    cout << ans;
 
     return 0;
 }
