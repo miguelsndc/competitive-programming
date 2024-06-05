4723d0169ce84b938c9d43195b88cbbe786ab611
miguelsndc <mglsndc@gmail.com>
2024-06-05 17:03:27 -0300

    suffering

--- CP/CF1612C.cpp
+++ CP/CF1612C.cpp
@@ -1,30 +1,38 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
-int f(int k) {
-};
+ll f(ll y) {
+    return y * (y + 1) / 2;
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
+    ll t, k, x;
     cin >> t;
-    while (t--) {
-        int k, x;
+    for (int i = 0; i < t; i++) {
         cin >> k >> x;
-        int low = 1;
-        int high = 2 * k - 1;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (f(mid)) {
-                high = mid;
+        ll l = 1, r = 2 * k - 1;
+        ll ans = 2 * k - 1;
+        int done = false;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (mid >= k) {  // second half up to mid
+                done = (f(k) + f(k - 1) - f(2 * k - 1 - mid)) >= x;
             } else {
-                low = mid + 1;
+                done = f(mid) >= x;  // first half
+            }
+            if (done) {  // we got a valid solution, however we keep iterating till the smallest one is found.
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
+        cout << ans << "\n";
     }
 
     return 0;
