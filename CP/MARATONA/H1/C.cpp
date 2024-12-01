b05acff0f1e1b5bc70e1212207e951f39c61d492
miguelsndc <mglsndc@gmail.com>
2024-12-01 15:06:47 -0300

    upsolving + l1

---
+++ CP/MARATONA/H1/C.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define all(a) (a.begin(), a.end())
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int, int>
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
