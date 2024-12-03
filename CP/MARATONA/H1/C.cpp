d6eff3b607e8ecc27ad96e7219b399c971d818a4
miguelsndc <mglsndc@gmail.com>
2024-12-03 10:46:30 -0300

    C

--- CP/MARATONA/H1/C.cpp
+++ CP/MARATONA/H1/C.cpp
@@ -15,8 +15,30 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n; cin >> n;
+
+    if (n == 1) {
+        int x; cin >> x;
+        cout << x;
+        return 0;
+    }
+
     vi a(n), b(n - 1);
     loop(i, 0, n) cin >> a[i];
     loop(i, 0, n - 1) cin >> b[i];
-    
+    sort(a.rbegin(), a.rend());
+    sort(b.rbegin(), b.rend());
+
+    int j = 0, c = 0, minbox = b[0];
+    loop(i, 0, n) {
+        while(j < n && a[j] > b[i]) {
+            minbox = a[j];
+            c++; j++;
+        }
+        j++;
+    }
+    if(c > 1) {
+        cout << -1;
+    } else {
+        cout << minbox;
+    }
 }
