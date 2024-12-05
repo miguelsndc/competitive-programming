0b4c0dc710c5d2c6cdbd79531b8fb690e5d3ff4d
Miguel Santos Nogueira da Costa <msnc@WINDOWS.CIN.UFPE.BR>
2024-12-05 14:00:16 -0300

    M

--- CP/MARATONA/H1/M.cpp
+++ CP/MARATONA/H1/M.cpp
@@ -16,5 +16,16 @@ int main()
     cin.tie(0);
     int tt; cin >> tt;
     while(tt--) {
-        
+        int n, m, L; cin >> n >> m >> L;
+        vector<array<int, 3>> hurdles(n);
+        vector<array<int, 3>> power_ups(m);
+        for (int i = 0; i < n; i++) {
+            cin >> hurdles[i][0] >> hurdles[i][1];
+            hurdles[i][2] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> power_ups[i][0] >> power_ups[i][1];
+            power_ups[i][2] = 1;
+        }
+    }
 }
