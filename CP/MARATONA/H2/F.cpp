3f2289f0c7ae68fb3adbff2394e843901172a847
miguelsndc <mglsndc@gmail.com>
2024-12-13 00:06:49 -0300

    F and B time limiting but we got this

--- CP/MARATONA/H2/F.cpp
+++ CP/MARATONA/H2/F.cpp
@@ -13,28 +13,38 @@ using namespace std;
 // f (k) k tamanho da string
 // retorna se existe caracter k dominante
 // monotonica pois sempre true a partir de um certo k (k <= tam da string)
-// passa em nlogn checagem + binsearch
+// passa em nlogn checagem * binsearch
+
+// abacaba
+// 
 
 bool ok(int k, string &s) {
 
-    int c = 0;
-    map<pair<char, int>, int> mp;
-    for (int i = 0; i <= s.size() - k; i++) {
-        c++;
-        string sub = s.substr(i, k);
-        for (int j = 0; j < sub.size(); j++) {
-            if (mp.find({i, sub[j]}) == mp.end()){
-                mp[{0, sub[j]}]++;
-                mp[{i, sub[j]}]++;
-            }
-        }
+    set<int> st;
+    st.insert(begin(s), end(s));
+
+    if (st.size() == s.size()) {
+        return (s.size()/ 2 + 1) < k;
     }
-    int maxc = -1;
-    for(auto [k, v]: mp) {
-        maxc = max(maxc, v);
+    else if (st.size() == 1) {
+        return 1 < k;
     }
 
-    return maxc >= c;
+    map<char, pair<int,int>> mp;
+    for (int i = 0; i < s.size(); i++) {
+        mp[s[i]] =  {i + 1, max(mp[s[i]].second, i + 1 - mp[s[i]].first)};
+    }
+
+    for (auto [key, v]: mp) {
+        mp[key].second = max(mp[key].second, abs(mp[key].first - (int)s.size()) + 1);
+    }
+
+    for (auto [_, v]: mp) {
+        if (v.second < k) {
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() 
@@ -46,5 +56,5 @@ int main()
     for (int b = s.size(); b >= 1; b /= 2) {
         while(!ok(x + b, s)) x += b;
     }
-    cout << x + 1;
+    cout << x;
 }
