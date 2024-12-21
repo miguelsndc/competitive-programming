d2a7b7581e926c9e9c76ca4c047fcd5ccae65265
msnc <msnc@g3c47.cin.ufpe.br>
2024-12-21 13:00:25 -0300

    A

--- CP/MARATONA/C4/A.cpp
+++ CP/MARATONA/C4/A.cpp
@@ -12,45 +12,26 @@ int main()
     cin.tie(0);
     int n, m; cin >> n >> m;
     vector<int> g[n];
+    vector<int> visited(n);
     for (int i = 0; i < m; i++)  {
         int a, b; cin >> a >> b; --a, --b;
         g[a].push_back(b);
         g[b].push_back(a);
     }
-
-    int currcomp = 0;
-    vector<int> components(n, -1);
-    vector<int> visited(n);
-    int mincomp = 0, minsize = INF;
+    int minn = INF, number = INF;
     for (int i = 0; i < n; i++) {
-        if (visited[i]) continue;
-        visited[i] = true;
-        components[i] = currcomp;
-        queue<int> q; 
-        q.push(i);
-        int size = 1;
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            for (int neighbour: g[node]) {
-                if (!visited[neighbour]) {
-                    size++;
-                    visited[neighbour] = true;
-                    components[neighbour] = currcomp;
-                }
-            }
+        if (g[i].size() < minn) {
+            minn = g[i].size();
+            number = i;
         }
-        if (size > minsize) {
-            minsize = size;
-            mincomp = currcomp;
-        }
-        currcomp++;
-    }
-    for (int i = 0; i < n; i++) {
-        components[i] = (components[i] != mincomp);
     }
 
-    for (int i = 0; i < n; i++) {
-        cout << components[i] << ' ';
+    visited[number] = true;
+    for (int i = 0; i < minn; i++) {
+        visited[g[number][i]] = true;
+    }
+
+    for (int v: visited) {
+        cout << !v << ' ';
     }
 }
