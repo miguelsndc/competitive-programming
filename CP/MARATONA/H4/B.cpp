b9e0deed4b6e781f1e8748d027f249756328e503
miguelsndc <mglsndc@gmail.com>
2024-12-21 23:24:08 -0300

    H4

--- CP/MARATONA/H4/B.cpp
+++ CP/MARATONA/H4/B.cpp
@@ -2,19 +2,7 @@
 
 using namespace std;
 #define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vb vector<bool>
-#define vs vector<string>
 #define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
-#define f first
-#define s second
-#define sp << " " <<
-#define spe << " "
 #define INF 1e18
 
 int main() {
