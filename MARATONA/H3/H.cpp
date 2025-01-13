4b34c88da909895b382f0a86a251f7c5c801900b
miguelsndc <mglsndc@gmail.com>
2025-01-13 14:43:11 -0300

    H ok

--- MARATONA/H3/H.cpp
+++ MARATONA/H3/H.cpp
@@ -5,63 +5,73 @@ using namespace std;
 #define ll long long
 #define MOD 1000000007
 #define INF 1000000009
-#define all(a) begin(a), end(a)
 
-void solve() {
-    int n, m; cin >> n >> m;
-    int start_y, start_x; cin >> start_y >> start_x;
-    int end_y, end_x; cin >> end_y >> end_x;
-    
-    int grid[105][105];
+int grid[105][105], visited[105][105];
+int n, m, rh, ch, rw, cw;
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
+vector<int> subset;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> grid[i][j];
+int bfs() {
+    for (int i = 0; i <= n + 1; i++) {
+        for (int j = 0; j <= m + 1; j++) {
+            visited[i][j] = false;
         }
     }
 
-    deque<pair<array<int, 2>, set<int>>> dq;
-    dq.push_back({{start_y, start_x}, set<int>{grid[start_y][start_x]}});
+    queue<pair<int, int>> q;
+    q.push({rh, ch});
+    visited[rh][ch] = true;
 
-    int dy[] = {0, 0, 1, -1};
-    int dx[] = {1, -1, 0, 0};
+    if (find(begin(subset), end(subset), grid[rh][ch]) == end(subset)) {
+        return false;
+    }
 
-    vector<vector<bool>> vis(n + 1, vector<bool>(m + 1));
-    vis[start_y][start_x] = true;
+    bool found = false;
 
-    int k = INT32_MAX;
+    while(!q.empty()) {
+        auto [y, x] = q.front();
+        q.pop();
 
-    while(!dq.empty()) {
-        int y = dq.front().first[0];
-        int x = dq.front().first[1];
-        set<int> v = set<int> {all(dq.front().second)}; // no maximo umas 34 operaçoes
-
-        dq.pop_front();
-
-        if(y == end_y && x == end_x) {
-            k = v.size();
+        if (y == rw && x == cw) {
+            found = true;
             break;
         }
 
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
+            if (ny < 1 || ny > n || nx < 1 || nx > m || visited[ny][nx]) continue;
 
-            if (ny < 1 || ny > n || nx < 1 || nx > n || vis[ny][nx]) continue;
-
-            vis[ny][nx] = true;
-            pair<array<int, 2>, set<int>> p = {{ny, nx}, set<int>{all(v)}};
-            p.second.insert(grid[ny][nx]);
-
-            if (p.second.size() == v.size()) {
-                dq.push_front(p);
-            } else {
-                dq.push_back(p);
+            if (find(begin(subset), end(subset), grid[ny][nx]) != end(subset)) {
+                q.push({ny, nx});
+                visited[ny][nx] = true;
             }
         }
     }
 
-    cout << k;
+    return found;
+}
+
+void solve() {
+    cin >> n >> m;
+    cin >> rh >> ch >> rw >> cw;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int ans = 10;
+    for (int i = 1; i <= (1 << 10); i++) {
+        for (int j = 0; j < 10; j++) {
+            int bit = i & (1 << j);
+            if (bit) subset.push_back(j + 1);
+        }
+        if (bfs()) ans = min(ans, (int)subset.size());
+        subset.clear();
+    }
+    cout << ans;
 }
 
 int main() 
