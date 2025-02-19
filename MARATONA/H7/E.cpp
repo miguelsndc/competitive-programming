cca468ec38ceaa8f2f14784ea0c7736d338624d6
miguelsndc <mglsndc@gmail.com>
2025-02-19 13:42:39 -0300

    bluh

--- MARATONA/H7/E.cpp
+++ MARATONA/H7/E.cpp
@@ -23,16 +23,16 @@ int power(int b, int p, int m) {
     return x;
 }
 
-const int m = 2;
+const int m = 4;
 class Matrix {
     public: 
-        ll mat[m][m] = {{0, 0}, {0, 0}};
-        Matrix operator*(const Matrix& p) {
+        ll mat[m][m] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        Matrix mul(const Matrix& p, int mod) {
             Matrix ans;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < m; j++) {
                     for (int k = ans.mat[i][j] = 0; k < m; k++) {
-                        ans.mat[i][j] = (ans.mat[i][j] + 1LL * (mat[i][k] % MOD) * (p.mat[k][j] % MOD)) % MOD;
+                        ans.mat[i][j] = (ans.mat[i][j] + 1LL * (mat[i][k] % mod) * (p.mat[k][j] % mod)) % mod;
                     }
                 }
             }
@@ -40,14 +40,14 @@ class Matrix {
         }
 };
 
-Matrix fexp(Matrix a, ll b) {
+Matrix fexp(Matrix a, ll b, int mod) {
     Matrix ans;
     for (int i = 0; i < m; i++) {
         ans.mat[i][i] = 1;
     }
     while(b) {
-        if (b & 1) ans = ans * a;
-        a = a * a;
+        if (b & 1) ans = ans.mul(a, mod);
+        a = a.mul(a, mod);
         b >>= 1;
     }
 
@@ -55,18 +55,31 @@ Matrix fexp(Matrix a, ll b) {
 }
 
 void solve() {
-    ll n; cin >> n;
+    ll n, mod; cin >> n >> mod;
     Matrix m;
-    m.mat[0][0] = m.mat[0][1] = m.mat[1][0] = 1;
-    auto ans = fexp(m, n);
-    cout << (ans.mat[0][0] * 0 + ans.mat[0][1] * 1) % MOD;
+    m.mat[0][0] = m.mat[0][1] = m.mat[0][2] = 1;
+    m.mat[1][0] = 1;
+    m.mat[2][2] = m.mat[2][3] = 1;
+    m.mat[3][3] = 1;
+    // 1 1 1 0
+    // 1 0 0 0 
+    // 0 0 1 1
+    // 0 0 0 1
+    if (n == 0) {
+        cout << 0 << '\n';
+    } else if (n == 1) {
+        cout << 1 << '\n';
+    } else {
+        m = fexp(m, n - 1, mod);
+        cout << (m.mat[0][0] + m.mat[0][2] + m.mat[0][3]) % mod << '\n';
+    }
 }
 
 int main() 
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int tt = 1; // cin >> tt;
+    int tt = 1; cin >> tt;
     for (int t = 1; t <= tt; t++) {
          solve();
     }
