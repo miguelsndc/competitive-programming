a31d6ab6735b7755147ad34dc67044077a0802cf
miguelsndc <mglsndc@gmail.com>
2025-03-04 16:06:52 -0300

    little upsolving

--- Solutions/Seletiva-2025/H6/H.cpp
+++ Solutions/Seletiva-2025/H6/H.cpp
@@ -1,62 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define ll long long int
-#define ld long double
 #define MOD 1000000007
 #define ii pair<int, int>
 #define vi vector<int>
-#define vll vector<ll>
-#define vb vector<bool>
-#define vs vector<string>
-#define vii vector<ii>
-#define vd vector<double>
-#define si set<int>
-#define mii map<int, int>
 #define fi first
 #define se second
-#define sz(x) int(x.size())
-#define all(a) (a).begin(), (a).end()
-#define pb emplace_back
-#define INF 1000000009
-bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+#define rep(var, k, n) for (int var = k; var < n; var++)
+#define se second
+#define int long long
+const int maxn = 2e5 + 5;
 
-const int MAX_DIGITS = 103;
-ll dp[MAX_DIGITS][11][2];
-vector<int> number;
-ll f(int pos, int prev, int smaller) {
-    if (pos == number.size()) return 1;
-    ll &ans = dp[pos][prev + 1][smaller];
-    if (~ans) return ans;
-    ans = 0;
-    for (int i=0; i <= (smaller ? 9: number[pos]); i++) {
-        bool smaller_now = (smaller || i < number[pos]);
-        if (i != prev) {
-            ans += f(pos + 1, i, smaller_now);
-        }
+// tamanho/digit anterior/zero a esquerda?/liberdade?
+int dp[20][10][2][2];
+int f(string &num, int n, int prev, int i, int leading_zero, int tight) {
+    if (i == n) return 1;
+    if (prev != -1 and dp[i][prev][leading_zero][tight] != -1) {
+        return dp[i][prev][leading_zero][tight];
     }
-    return dp[pos][prev + 1][smaller] = ans;
-}
-vi ntovec(int num)  {
-    if (num == 0) return {0};
-    vi v;
-    for (; num >0; num /= 10) v.push_back(num % 10);
-    reverse(begin(v), end(v));
-    return v;
-}
-
-ll solve(int n) {
-    if (n < 0) return 0;
-    number = ntovec(n);
-    memset(dp, -1, sizeof dp);
-    return f(0, -1, false);
+    int low = 0;
+    int high = tight ? (num[i] - '0') : 9;
+    int ans = 0;
+    rep(dig, low, high+1) {
+        if (dig == prev and dig != 0) continue;
+        if (dig == prev and dig == 0 and not leading_zero) continue;
+        ans += f(num, n, dig, i+1, leading_zero&dig==0, tight&(dig==high));
+    }
+    return dp[i][prev][leading_zero][tight] = ans;
 }
 
-int main() 
+signed main() 
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int a, b; cin >> a >> b;
-    cout << solve(b) - solve(a - 1);
+    string a,b;cin>>a>>b;
+    memset(dp, -1, sizeof dp);
+    int ans = f(b,b.size(),-1,0,1,1);
+    memset(dp, -1, sizeof dp);
+    ans -= f(a,a.size(),-1,0,1,1);
+    bool valid=true;
+    rep(i,1,a.size()){
+        if (a[i-1]==a[i]) {
+            valid=false;break;
+        }
+    }
+    ans+=valid;
+    cout << ans << '\n';
 }
